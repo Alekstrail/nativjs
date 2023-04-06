847c46763b4c23fb6d9e2d04a6ec16ae646d984a
Alekstrail <strukov_alexanderunner@hotmail.com>
2023-04-06 22:37:29 +0300

    1

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {splitIntoWorlds} from "./testsles/01";
+
+const sentense = 'hello my friends!';
+const result = splitIntoWorlds(sentense);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/testsles/01.test.ts
@@ -0,0 +1,22 @@
+import {mult, sum} from "./01";
+
+test('sum should be correct', () => {
+    let a =1;
+    let b =2;
+    let c =3;
+    const result1 = sum(a,b);
+    const result2 = sum(b,c);
+     //expect result
+    expect(result1).toBe(3);
+    expect(result2).toBe(5);
+})
+test('sum should be correct', () => {
+    let a =1;
+    let b =2;
+    let c =3;
+    const result1 = mult(a,b);
+    const result2 = mult(b,c);
+     //expect result
+    expect(result1).toBe(2);
+    expect(result2).toBe(6);
+})
